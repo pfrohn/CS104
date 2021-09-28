eebc92f9c095e1f0a3197e204990ecfc703b1382
pfrohn <91150762+pfrohn@users.noreply.github.com>
2021-09-28 14:48:26 -0400

    Average an Algorithm code with while loop

---
+++ Average.py
@@ -0,0 +1,26 @@
+average = 0
+total = 0
+howManyEntered = 0
+
+howMany = int(input('How many test scores would you like to average? '))
+
+testScore = int(input('Enter test score: '))
+
+total = total + testScore
+
+howManyEntered = howManyEntered + 1
+
+
+while howManyEntered < howMany:
+    testScore = int(input('Enter test score: '))
+
+    total = total + testScore
+
+    howManyEntered = howManyEntered + 1
+
+
+average = total / howMany
+
+print ('The average for the test scores you entered is: ')
+print(average)
+    
